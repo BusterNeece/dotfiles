ad09581c41366488dcf006d111294ac7011c7cf1
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-04-26 22:01:19 -0500

    Update deploy.ps1

--- .posh/deploy.ps1
+++ .posh/deploy.ps1
@@ -5,14 +5,14 @@ if((Get-ExecutionPolicy) -ne 'Unrestricted')
 }
 
 Function Install-Scoop {
-	[Environment]::SetEnvironmentVariable("SCOOP", "$HOME\.scoop", "User")
-
-	Invoke-WebRequest 'https://get.scoop.sh' | Invoke-Expression
-
-	'scoop install Git-with-OpenSSH Sudo Which --global' | Set-Content -path temp_script.ps1
-	Start-Process PowerShell -verb RunAs -argument "-noProfile $(Convert-Path .\temp_script.ps1)"
-	Remove-Item temp_script.ps1 -force
-
+	if ((Get-Command 'scoop' -errorAction SilentlyContinue) -eq '')
+	{
+		[Environment]::SetEnvironmentVariable("SCOOP", "$HOME\.scoop", "User")
+		iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+	}
+	
+	scoop install git
+	scoop install openssh sudo which
 	scoop bucket add Extras
 }
 
@@ -39,16 +39,6 @@ Function Install-ScoopPackages {
 	$packages | ForEach { scoop install $_ }
 }
 
-$chocoPackages = @(
-	'googlechrome',
-	'google-backup-and-sync',
-	'jetbrainstoolbox',
-	'vnc-viewer',
-	'authy-desktop',
-	'toggl',
-	'plexmediaplayer'
-)
-
 $scoopPackages = @(
 	'7zip',
 	'cmder',
@@ -59,7 +49,6 @@ $scoopPackages = @(
 	'greenshot',
 	'firefox-developer',
 	'audacity',
-	'mpc-hc',
 	'hyper',
 	'notepad2',
 	'heidisql',
@@ -67,6 +56,18 @@ $scoopPackages = @(
 	'speccy',
 	'etcher',
 	'discord'
+)
+
+$chocoPackages = @(
+	'googlechrome',
+	'google-backup-and-sync',
+	'jetbrainstoolbox',
+	'vnc-viewer',
+	'authy-desktop',
+	'toggl',
+	'plexmediaplayer',
+	'k-litecodecpackfull',
+	'shutup10',
 )
 
 Install-Scoop
