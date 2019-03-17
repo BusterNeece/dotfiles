7a4b9b50c6d23106a8922cd3957f04f933cd0710
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-03-17 16:38:34 -0500

    Update ubuntu_provisioning.sh

--- ubuntu_provisioning.sh
+++ ubuntu_provisioning.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 # Core OS-level software
-sudo apt-get install -y git sudo curl wget vim arc-theme gnome-tweak-tool
+sudo apt-get install -y git sudo curl wget vim arc-theme gnome-tweak-tool fonts-firacode
 
 sudo apt-get autoremove
 
@@ -18,6 +18,33 @@ sudo snap install phpstorm --classic
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Install Docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
+
+sudo usermod -aG docker `whoami`
+
+# Install Docker Compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Use local clock at system level
+timedatectl set-local-rtc 1 --adjust-system-clock
+
+# Increase inotify watches limit
+echo "fs.inotify.max_user_watches = 524288" | sudo tee /etc/sysctl.d/jetbrains.conf
+sudo sysctl -p --system
+
+# Fix Discord audio
+sudo sed -i 's/load-module module-udev-detect/load-module module-udev-detect tsched=0/' /etc/pulse/default.pa
+pulseaudio -k && sudo alsa force-reload
+
+# Fix Spotify HiDPI
+sudo sed -i 's/%U/--force-device-scale-factor=2.0 %U/' /var/lib/snapd/desktop/applications/spotify_spotify.desktop
 
 # Code repositories
 mkdir -p ~/Documents/Web/AzuraCast
@@ -35,18 +62,4 @@ cd AzuraCast
 cp azuracast.dev.env azuracast.env
 cp docker-compose.dev.yml docker-compose.yml
 
-cd ~
-
-# Use local clock at system level
-timedatectl set-local-rtc 1 --adjust-system-clock
-
-# Increase inotify watches limit
-echo "fs.inotify.max_user_watches = 524288" | sudo tee /etc/sysctl.d/jetbrains.conf
-sudo sysctl -p --system
-
-# Fix Discord audio
-sudo sed -i 's/load-module module-udev-detect/load-module module-udev-detect tsched=0/' /etc/pulse/default.pa
-pulseaudio -k && sudo alsa force-reload
-
-# Fix Spotify HiDPI
-sudo sed -i 's/%U/--force-device-scale-factor=2.0 %U/' /var/lib/snapd/desktop/applications/spotify_spotify.desktop
+echo "Initial provision complete. Reboot necessary for Docker."
