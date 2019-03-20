ab8d66594bdf90bad1978f7683e45a2f60455c1a
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-03-20 03:16:43 -0500

    Update ubuntu_provisioning.sh

--- ubuntu_provisioning.sh
+++ ubuntu_provisioning.sh
@@ -24,6 +24,8 @@ rm google-chrome-stable_current_amd64.deb
 # Install GitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
+sudo apt-get install -f -y
+
 rm gitkraken-amd64.deb
 
 # Install Docker
