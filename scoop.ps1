227ec8598fd954b68128693669d1cb081904a98c
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-11-11 02:15:16 -0600

    Update scoop.ps1

--- scoop.ps1
+++ scoop.ps1
@@ -1,13 +1,13 @@
 # Install "Developer Mode" tools first and set execution policy to RemoteSigned
 
-iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-
 # If using the W: second partition
 $env:SCOOP='W:\Scoop'
 [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
 
 $env:SCOOP_GLOBAL='W:\ScoopGlobal'
 [Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
+
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 scoop install --global git openssh sudo which aria2
 
