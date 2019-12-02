9e810232fb5a04b2ee9c06d4a9da5a332bfaa604
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2019-12-01 18:03:09 -0600

    Update scoop.ps1

--- scoop.ps1
+++ scoop.ps1
@@ -1,19 +1,14 @@
 # Install "Developer Mode" tools first and set execution policy to RemoteSigned
 
-# If using the W: second partition
-$env:SCOOP='W:\Scoop'
-[Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
-
-$env:SCOOP_GLOBAL='W:\ScoopGlobal'
-[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
-
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
-scoop install --global git openssh sudo which aria2
+scoop install git openssh sudo which aria2
 
 # Often installed via Scoop
 scoop bucket add extras
 scoop install 7zip cmder gitkraken speccy etcher postman
 
 # Sometimes installed via Scoop or Ninite
-scoop install vscode jetbrains-toolbox
+scoop install jetbrains-toolbox
+
+# Also available: vscode
